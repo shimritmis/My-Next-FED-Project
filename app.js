7d38c931fbf059fd34a63e90c51e28c114f85768
Shimrit Misgav <shimritmis@gmail.com>
2018-11-29 17:25:14 +0200

    body parser added

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
 const express = require ('express');
 const exphbs = require ('express-handlebars');
+const bodyParser = require ('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
 
 
-//Map global promise- get rid of warning
+// Map global promise- get rid of warning
 mongoose.Promise = global.Promise;
-//connect to mongoose
+
+// Connect to mongoose
 mongoose.connect('mongodb://localhost/vidjot-dev')
 .then(()=> console.log('MongoDB Connected...'))
 .catch(err=>console.log(err));
 
-//Load Idea Model 
+// Load Idea Model 
 require('./models/Idea');
 const Idea = mongoose.model('ideas');
 
-//Handlebars Middleware
-
+// Handlebars Middleware
 app.engine('handlebars', exphbs ({
     defaultLayout:'main'
 }));
 app.set('view engine', 'handlebars'); 
 
-//How middleware works
-// app.use(function(req,res,next){
-//     console.log(Date.now());
-//     req.name = "Shimrit Misgav";
-//     next();
-// })
+// Body parser middleware -access whatever submitted by req.body, for example- in order to get form values.
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 
 //Index Route 
@@ -38,15 +36,22 @@ app.get('/', (req, res)=> {
         title:title
     });
 });
+
 //About Route
 app.get('/about', (req,res)=> {
     res.render('about');
-})
+});
 
 //Add Idea Form
 app.get('/ideas/add', (req,res)=> {
     res.render('ideas/add');
-})
+});
+
+//Process Form
+app.post('/ideas', (req,res)=> {
+    console.log(req.body);
+    res.send('ok')
+});
 
 
 const port = 5000;
