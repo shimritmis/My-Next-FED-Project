72e83331bd595c73f04f6ffe7e297b66d9621e2a
Shimrit Misgav <shimritmis@gmail.com>
2018-12-01 13:47:12 +0200

    user registration

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt= require('bcryptjs');
+// const passport = require('passport');
 const router = express.Router();
+
+// Load User Model
+require('../models/User');
+const User = mongoose.model('users'); 
 
 // User Login Route
 router.get('/login', (req, res) => {
@@ -30,8 +36,43 @@ router.post('/register', (req, res)=> {
             password2: req.body.password2
         })
     } else {
-        res.send('passed');
+        User.findOne({email: req.body.email})
+            .then(user => {
+                if(user){
+                    req.flash('error_msg', 'Email already registered');
+                    res.redirect('/users/login');
+                } else {
+                    const newUser = new User({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: req.body.password
+                    });
+                    bcrypt.genSalt(10, (err, salt)=> {
+                        bcrypt.hash(newUser.password, salt, (err, hash)=> {
+                            if(err) throw err;
+                            newUser.password = hash;
+                            newUser.save()
+                                .then (user=> {
+                                    req.flash('success_msg', 'You are now registered and can log in!');
+                                    res.redirect('/users/login'); 
+                                })
+                                .catch(err=> {
+                                    console.log(err);
+                                    return; 
+                                })
+                        });
+                    });
+                }
+            })
     }
 })
 
 module.exports = router;
+
+/* THE HASH PASSWORD, FROM NPM: 
+var bcrypt = require('bcryptjs');
+bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash("B4c0/\/", salt, function(err, hash) {
+        // Store hash in your password DB.
+    });
+}); */
