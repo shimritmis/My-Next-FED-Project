9c3c177b4689dd22878866e78bc4f4638eeaccba
Shimrit Misgav <shimritmis@gmail.com>
2018-12-01 16:16:05 +0200

    Local Strategy

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
 const bodyParser = require ('body-parser');
+const passport = require('passport');
 const mongoose = require('mongoose');
 // const MongoClient = require('mongodb').MongoClient;
 
@@ -13,6 +14,9 @@ const app = express();
 // Load Routes
 const ideas = require('./routes/ideas');
 const users = require('./routes/users');
+
+// Passport Config 
+require('./config/passport')(passport); 
 
 // Map global promise- get rid of warning
 mongoose.Promise = global.Promise;
