9f5e34519d11073f96fc3addb07f33c81f19ff79
Shimrit Misgav <shimritmis@gmail.com>
2018-12-01 17:21:03 +0200

    adding login functionality

--- config/passport.js
+++ config/passport.js
@@ -7,6 +7,13 @@ const User = mongoose.model('users');
 
 module.exports = function(passport){
   passport.use(new LocalStrategy({usernameField: 'email'}, (email, password, done) => {
-    console.log(email);
+    User.findOne({
+        email:email
+    }).then(user => {
+        if(!user){
+            return done(null, false, {message: 'No User Found'});
+        }
+    })
   }));
 }
+
